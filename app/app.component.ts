1bff5f02d3723f567055a6981146e73e1b2a01df
patrick s <patrick s>
2016-12-28 13:44:20 -0800

    refactor into app.component.html

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'mt-app',
-  template: `<mt-navbar></mt-navbar>`
+  templateUrl: 'app.component.html'
 })
+
 export class AppComponent { }
